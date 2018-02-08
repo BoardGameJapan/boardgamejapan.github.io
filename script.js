248a2e3864a7aa63e3f49d61f2d798cc5d0f5f7c
saireya <saireya@gmail.com>
2018-02-08 11:43:06 +0900

    Added Atom Feed

--- script.js
+++ script.js
@@ -6,9 +6,34 @@ var data = {
 		'works',
 		'contact',
 	],
+	news: [],
 };
 
 var app = new Vue({
 	el: '#app',
 	data: data,
 });
+
+// feedを読み込む
+function getFeedEntries(url) {
+	fetch(url)
+		.then(res => res.text())
+		.then(text => {
+			var parser = new DOMParser();
+			var xml = parser.parseFromString(text, "text/xml");
+			var entries = xml.getElementsByTagName("entry");
+			for (var i = 0; i < entries.length; i++) {
+				data.news.push({
+					title: getChildTag(entries, i, "title").textContent,
+					link: getChildTag(entries, i, "link").getAttribute("href"),
+					updated: getChildTag(entries, i, "updated").textContent.split('T')[0],
+					summary: getChildTag(entries, i, "summary").textContent,
+				});
+			}
+		});
+}
+getFeedEntries("./feed.atom");
+
+function getChildTag(e, i, tagname) {
+	return e[i].getElementsByTagName(tagname)[0];
+}
